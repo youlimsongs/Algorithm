2fd14310e96d3bdf60b68134f789cf1c9b56970f
Yourim Song <yourim.song.00@gmail.com>
2023-12-31 14:21:36 +0900

    [Bronze I] Title: 롤 케이크, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3985. 롤 케이크/롤 케이크.py
+++ 백준/Bronze/3985. 롤 케이크/롤 케이크.py
@@ -1,34 +1,35 @@
 import sys
+sys_input=sys.stdin.readline
 
-length = int(input())
-n = int(input())
+length = int(sys_input())
+n = int(sys_input())
+
 cake = [0]*length
-
-prev = 0
-exp_num = 0
-real_num = 0
+exp_num, real_num = 0, 0
+prev, temp = 0, 0
 
 for i in range(1,n+1):
-    start, end = map(int, sys.stdin.readline().split())
+    start, end = map(int, sys_input().split())
 
     # 가장 많은 조각을 받을 것으로 기대
     if(prev < end-start): 
         prev = end-start
         exp_num = i    
 
-    # 실제로 가장 많은 조각
-    for j in range(start-1, end):
-        if(cake[j]==0): cake[j]=i
     
-# print(cake)
-
-temp=0
-
-for k in range(1, n+1):
-    cur = cake.count(k)
-    if(temp < cur):
-        real_num = k
-        temp = cur
+    # 실제로 가장 많은 조각
+    cnt = 0
+    for j in range(start-1, end):
+        if(cake[j]==0): 
+            cake[j]=i
+            cnt += 1
+    
+    
+    if(cnt > temp):
+        # print(cnt)
+        # print(temp)
+        real_num =i
+        temp = cnt
     
 print(exp_num)
 print(real_num)
