7e50727fc2e891eae818fb1f04457f6a114c040a
Yourim Song <yourim.song.00@gmail.com>
2024-02-06 02:39:21 +0900

    [Bronze II] Title: 분해합, Time: 1232 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(1,n+1):
+    num = sum((map(int, str(i))))
+    res = i+num
+    if(res == n):
+        print(i)
+        break
+    if(i==n):
+        print(0)
