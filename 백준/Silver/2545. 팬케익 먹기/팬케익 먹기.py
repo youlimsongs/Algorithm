40309c01b9171d442e7ea4e449c82f28ff7a24e1
Yourim Song <yourim.song.00@gmail.com>
2024-02-27 03:12:46 +0900

    [Silver V] Title: 팬케익 먹기, Time: 68 ms, Memory: 31120 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.py
+++ 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.py
@@ -1,18 +1,15 @@
-import sys
-input = sys.stdin.readline
-
 t = int(input())
-for _ in range(t):
+for i in range(t):
     input()
     a,b,c,d = map(int, input().split())
 
-    for i in range(d):
-        if max(a,b,c) == a :
-            a -= 1
-        elif max(a,b,c) == b :
-            b -= 1
-        else:
-            c -= 1
-    
-    # print(a,b,c)
-    print(a*b*c)
+    a, b, c = sorted((a, b, c))
+
+    #남은 길이의 합
+    s = a + b + c - d
+
+    tmp = min(s // 3, a) #남은 길이를 abc가 최대한 동일하게 가져가도록, 하지만 평균 길이보다 a가 더 작을 수 있음
+    a = tmp #a 길이 설정
+    s -= tmp #남은 b,c길이 합
+    b = min(s // 2, b) #b,c길이 되도록 동일하게 b길이 설정
+    print(a*b*(s-b))
