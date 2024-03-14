e4ebca0b5cc47d730da8403795a1ddc9c2e24f9b
Yourim Song <yourim.song.00@gmail.com>
2024-03-14 20:12:52 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -3,10 +3,9 @@ str = input().strip()
 
 r, M = 31, 1234567891
 
-# print(ord("a"))
 res = 0
 for i in range(l):
     s = ord(str[i]) - 96
-    res += s * (r**i) % M
+    res += s * (r**i)
 
-print(res)
+print(res % M)
